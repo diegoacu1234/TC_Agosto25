91d01bef464b986d3ff8a42f9f161d3d6b0ec330
Diego Acuña <127145418+diegoacu1234@users.noreply.github.com>
2025-03-26 16:02:25 -0300

    Actualizar expArit.hs

--- S02_Practico0/expArit.hs
+++ S02_Practico0/expArit.hs
@@ -26,7 +26,7 @@ eval :: Mem -> Exp -> (Mem, Val)
 eval m (Lit b) = (m, b)
 -- Cada regla de las siguientes 3 se escribió de forma distinta para ver conceptos nuevos de Haskell:
 eval m (Sum e1 e2) = case eval m e1 of
-  (m', b1) -> case eval m e2 of
+  (m', b1) -> case eval m' e2 of
     (m'', b2) -> (m'', b1 + b2) -- esta fue con case, como toda la vida
 eval m (Mul e1 e2) =
   let (m', b1) = eval m e1 -- ver que "asigno" variables y luego las puedo usar
